ef83da5fa04b2679fe2938b24807c0bde5f2b3d8
Lucy Wall <lhwall@gmail.com>
2018-07-13 13:15:14 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,5 +12,11 @@ class RecipesController < ApplicationController
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
   end
 end
+
+private
+
+def recipe_params
+  params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
